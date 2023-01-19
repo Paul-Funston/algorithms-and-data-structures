1e3d6048d9c4610364d81e5e40d38a5f7d767430
Paul Funston <paulfunston9@gmail.com>
2023-01-18 19:21:30 -0600

    edited complexity comments

--- AlgorithmsAndDataStructures_Lab4/Program.cs
+++ AlgorithmsAndDataStructures_Lab4/Program.cs
@@ -3,29 +3,6 @@
 Note that several of these methods have Nested Lists (ie. Lists within Lists) as their parameters. It is best to write helper methods to simplify the required ones. 
 For example, int GetHighestValueInList(List<int>) would be a helpful method to write and use throughout these problems. 
 Ideally, each function should be as simple as possible, performing a small amount of work before passing it to another function inside or after that method.
-
-
-
-    List<int> MaxNumbersInLists(List<List<int>>) accepts as a parameter a List of Lists of Integers. It returns a new list with each element representing the maximum number found in the corresponding original list. 
-
-        For example, { {1, 5, 3}, {9, 7, 3, -2}, {2, 1, 2} } returns {5, 9, 2}. Output the results with a message like: “List 1 has a maximum of 5. List 2 has a maximum of 9. List 3 has a maximum of 2.”
-
-
-
-    String HighestGrade(List<List<int>>) accepts a list of number grades among students in different courses (where each list represents a single course), between 0 and 100. It returns the highest grade among all students in all courses.
-
-        For example: { {85,92, 67, 94, 94}, {50, 60, 57, 95}, {95} } returns "The highest grade is 95. This grade was found in class(es) {index}".
-
-
-
-    List<int> OrderByLooping (List<int>) orders a list of integers, from least to greatest, using only basic control statements (ie. if/else, for/while).
-
-        For example, argument {6, -2, 5, 3} returns {-2, 3, 5, 6}.
-
-Bonus:
-
-Once you finish these methods, do some research into bubble sorting. refactor OrderByLooping method 
- 
  
  */
 
@@ -45,8 +22,6 @@ List<List<int>> listNumbers = new List<List<int>>();
 listNumbers.Add(new List<int> { 9, 7, 3, -2 });
 listNumbers.Add(new List<int> { 2, 1, 2 });
 listNumbers.Add(new List<int> { 1, 5, 3 });
-
-
 
 int[] max = new int[listNumbers.Count()];
 int maxIndex = 0;
@@ -105,7 +80,7 @@ foreach (List<int> list in listGrades)
 Console.Write($"The highest grade is {overalHighestGrade}. This grade was found in class(es) ");
 
 
-// Return to make a better way of getting the classes with the grade
+// Return to find a better way of getting the classes with highest grade
 for (int i = 0; i < highestGrades.Length; i++)
 {
     if (highestGrades[i] == overalHighestGrade)
@@ -123,7 +98,7 @@ Console.WriteLine();
      List<int> OrderByLooping (List<int>) orders a list of integers, from least to greatest, using only basic control statements (ie. if/else, for/while).
         For example, argument {6, -2, 5, 3} returns {-2, 3, 5, 6}.
 
-Complexity first Attempt is ON^2 and doesn't handles duplicates poorly 
+Complexity first Attempt is ON^2 and handles duplicates poorly 
 
 */
 
@@ -182,8 +157,6 @@ for (int j = 0; j < bubbleSort.Length; j++)
         }
     }
 }
-
-
 
 
 foreach(int i in bubbleSort)
